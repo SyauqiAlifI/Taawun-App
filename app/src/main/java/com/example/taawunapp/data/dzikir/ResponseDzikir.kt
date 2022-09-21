991f263d40767e0356643fa763a4afe281f15461
haydarahmad <fattanhay0@gmail.com>
2022-09-21 13:07:02 +0700

    API

--- app/src/main/java/com/example/taawunapp/data/dzikir/ResponseDzikir.kt
+++
@@ -1,9 +0,0 @@
-package com.example.taawunapp.data.dzikir
-
-import com.google.gson.annotations.SerializedName
-
-data class ResponseDzikir(
-
-	@field:SerializedName("Response")
-	val response: List<ResponseItemDzikir?>? = null
-)
