89eed01d3066d9b5117004b3d2f73cc0e3bfc580
haydarahmad <fattanhay0@gmail.com>
2022-09-21 13:41:54 +0700

    API

--- app/src/main/java/com/example/taawunapp/network/ApiConfig.kt
+++ app/src/main/java/com/example/taawunapp/network/ApiConfig.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import java.util.concurrent.TimeUnit
 
 object ApiConfig1 {
@@ -38,6 +39,9 @@ object ApiConfig2{
 
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL_B)
-
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
     }
 }
